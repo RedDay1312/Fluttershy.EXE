c2b26ec46e1049b52365f2e7cd627a732413a1fd
RedDay1312 <xxmonkexx13@gmail.com>
2026-09-03 13:20:44 +0300

    REBUILD: replace Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,2 @@
 import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './',
-  build: {
-    target: 'es2020',
-    sourcemap: false,
-    emptyOutDir: true,
-  },
-});
+export default defineConfig({base:'./',build:{target:'es2022',sourcemap:false,assetsDir:'assets',emptyOutDir:true},server:{strictPort:true,port:5173,host:'127.0.0.1'},preview:{strictPort:true,port:4173,host:'127.0.0.1'}});
